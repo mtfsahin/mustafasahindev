8504423d82fbb5043662c978b50715c6518dbcc2
mtfsahin <mustafa9889.ma@gmail.com>
2022-12-12 01:00:00 +0300

    react-ts particles kurulumu

--- src/Router.js
+++ src/Router.js
@@ -2,20 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import App from './App';
 import Home from './Home';
 
+
 const router=createBrowserRouter([
     {
         path:'/',
         element:<App />,
         errorElement: <div>Hatasayfası </div>,
-        children:[
-            {
-                path:'deneme',
-                element:<div>Deneme sayfası</div>
-            },{
-                path:'test',
-                element:<div>TEst sayfası</div>
-            }
-        ]
     },
     {
         path:'/home',
